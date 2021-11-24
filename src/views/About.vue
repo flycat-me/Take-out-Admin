7143d1028eb4ac043b659e242d6b8f7caebc3f4f
terrykali <terry1978kali@outlook.com>
2021-11-24 14:59:15 +0800

    update

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <sidebar></sidebar>
+</template>
+
+<style>
+</style>
+
+<script>
+import Sidebar from '../components/Sidebar.vue'
+  export default {
+  components: { Sidebar },
+
+  }
+</script>
---
+++ src/views/login/login.vue
@@ -0,0 +1,42 @@
+<template>
+<div id="login">
+    <h2 class="loginbox-title">用户名账号登录</h2>
+    <div class="g-bd">
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'login'
+
+}
+</script>
+
+<style>
+#login {
+    width: 400px;
+    height: 540px;
+    background-color: gray;
+
+}
+.loginbox-title {
+    padding: 40px 0 0;
+    color: #333;
+    font-size: 20px;
+    line-height: 28px;
+    text-align: center;
+}
+h2 {
+    display: block;
+    font-weight: bold;
+}
+.g-bd {
+    width: 400px;
+    height: 302px;
+    overflow: visible;
+    position: relative;
+    border-radius: 4px;
+    background: #fff;
+}
+</style>
